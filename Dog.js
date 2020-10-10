a44ffb299b3126bf455927ed66f7f64472d17d52
lmquan1609 <luuminhquan155@gmail.com>
2020-10-10 15:07:11 +0700

    Change git commands

--- Dog.js
+++ Dog.js
@@ -2,6 +2,9 @@ function Dog() {
     this.stomach = [];
 }
 
+// Add more comments to try out the git reset --soft
 Dog.prototype.eat = function (cat) {
     this.stomach.push(cat);
 };
+
+module.exports = Dog;
