03f2f2ecef662f46ed60fff73ffc6756b1e38b9e
lmquan1609 <luuminhquan155@gmail.com>
2020-10-10 15:14:37 +0700

    Revert Dog Module

--- Dog.js
+++ Dog.js
@@ -1,10 +1,3 @@
 function Dog() {
-    this.stomach = [];
+
 }
-
-// Add more comments to try out the git reset --soft
-Dog.prototype.eat = function (cat) {
-    this.stomach.push(cat);
-};
-
-module.exports = Dog;
